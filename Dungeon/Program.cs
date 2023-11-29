81b33942f7b822a844326d553685ab7a4f26c800
Allen Taylor <allen.taylor0625@gmail.com>
2023-11-29 10:23:19 -0600

    Completed Block 2 labs

--- Dungeon/Program.cs
+++ Dungeon/Program.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DungeonLibray;
+
 
 namespace DungeonApplication
 {
@@ -26,13 +28,22 @@ namespace DungeonApplication
             #region Weapon Object Creation
 
             //TODO Create a Weapon
+            Weapon w1 = new Weapon("Dragon Slayer", 45, 5, 15, true);
+
+            // Console.WriteLine($"\nWeapon Name:{w1.Name}\nDamage:{w1.MaxDamage}-{w1.MinDamage}\nBonus Hit Chance: {w1.BonusHitChance}%\n" +
+            //   $"Handedness: {w1.IsTwoHanded}");
+            Console.WriteLine(w1);
 
             #endregion
 
             #region Player Object Creation
 
             //TODO Create a Player (and equip them with a weapon)
+            Character c1 = new Character("John Marston", 100, 25, 15, 75);
 
+            //Console.WriteLine($"Name: {c1.Name}\nMax: Life: {c1.MaxLife}\nHit Chance: {c1.HitChance}\nBlock: {c1.Block}" +
+            //  $"\nMax Life: {c1.Life}\n");
+            Console.WriteLine(c1);
             #endregion
 
             #region Main Game Loop
@@ -142,7 +153,7 @@ namespace DungeonApplication
                 "Imagine stepping into a room shrouded in darkness. The walls are adorned with ancient, cobweb-covered paintings that seem to follow your every move.",
                 "Imagine stepping into a cozy room with soft lighting, comfy furniture, and beautiful artwork. It's a perfecft space to relax and unwind, surrounded by a warm and inviting atmosphere.",
                 "Picture stepping into a dimly lit room, with flickering candles casting eerie shadows on the walls. The air feels heavy, and you can hear faint whispers echoing in the distance. ",
-                "Imagine stepping into a room that seems frozen in time. The walls are peeling wallpaper, revealing glimpses of faded portraits."
+                "Imagine stepping into a room that seems frozen in time. The walls are peeling wallpaper, revealing glimpses of faded portraits. The floor creaks under your every step, as if it's whispering secerts."
             };
 
             Random random = new Random(); 
---
+++ DungeonLibray/Weapon.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DungeonLibray
+{
+    public class Weapon
+    {
+        //Fields
+        private int _minDamage;
+        private int _maxDamage;
+        private string _name;
+        private int _bonusHitChance;
+        private bool _isTwoHanded;
+
+        //Properties
+        public int MaxDamage
+        {
+            get { return _maxDamage; }
+            set { _maxDamage = value; }
+
+        }
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+        public int BonusHitChance
+        {
+            get { return _bonusHitChance; }
+            set { _bonusHitChance = value; }
+        }
+        public bool IsTwoHanded
+        {
+            get { return _isTwoHanded; }
+            set { _isTwoHanded = value; }
+        }
+        public int MinDamage
+        {
+            get { return _minDamage; }
+            set
+            {
+                if (value >0 && value <= MaxDamage)
+                {
+                    _minDamage = value;
+                }
+                else
+                {
+                    _minDamage = 1;
+                }
+            }
+        }
+        //Constructors
+        public Weapon (string name, int maxDamage, int minDamage, int bonusHitChance, bool isTwoHanded)
+        {
+            MaxDamage = maxDamage;
+            MinDamage = minDamage;
+            Name = name;
+            BonusHitChance = bonusHitChance;
+            IsTwoHanded = isTwoHanded;
+        }
+        //Methods
+        public override string ToString()
+        {
+            string handedness = IsTwoHanded ? "Two-Handed" : "One-Handed";
+            return $"Name:{Name}, Damage:{MinDamage}-{MaxDamage}, Bonus Hit Chance: {BonusHitChance}," +
+                $"Handedness: {handedness}";
+        }
+    }
+
+}
